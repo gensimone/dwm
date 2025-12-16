358c881bbb6e94b08df45f5314cd59b438482afc
Simone Gentili <gensimone.git@gmail.com>
2025-12-16 20:29:02 +0100

    Update config.def.h colorscheme. Add gapx field

--- config.def.h
+++ config.def.h
@@ -1,17 +1,18 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 6;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappx     = 15;       /* gap pixel between windows */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=12" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=12";
-static const char col_gray1[]       = "#002b36";
-static const char col_gray2[]       = "#073642";
-static const char col_gray3[]       = "#839496";
-static const char col_gray4[]       = "#93a1a1";
-static const char col_cyan[]        = "#174956";
+static const char *fonts[]          = { "ProFont IIx Nerd Font:size=12" };
+static const char dmenufont[]       = "ProFont IIx Nerd Font:size=12";
+static const char col_gray1[]       = "#1a1a1a";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#1a1a1a";
+static const char col_cyan[]        = "#969896";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
